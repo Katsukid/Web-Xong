3a5315699341c638b4903414bf8b87f8e133f1ff
Trần Nam Khánh <noangel0607@gmail.com>
2019-01-07 09:06:07 +0700

    xong

---
+++ Web-completed/BookMVC/Models/AccountModelView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace BookMVC.Models
+{
+     public class AccountModelView
+     {
+          [Required]
+          public string Email { get; set; }
+
+          [Required]
+          public string Password { get; set; }
+
+          [DefaultValue(true)]
+          [Display (Name ="Nhớ tài khoản?")]
+          public bool RememberMe { get; set; }
+     }
+}
